916ec0c198b0340fbbf5f76901048c08e3c0595c
MANIKANTA KORIMILLI <112791483+belvin011@users.noreply.github.com>
2022-11-23 14:45:53 +0530

    Add files via upload

---
+++ CC.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+const int M=1e9+7;
+long long Mod(long long x){
+    return ((x%M + M)%M); }
+long long Add(long long a, long long b){
+    return Mod(Mod(a)+Mod(b)); }
+long long Mul(long long a, long long b){
+    return Mod(Mod(a)*Mod(b)); }
+    
+int main(){
+   
+
+    
+    
+    
+    
+
+
+}
+
